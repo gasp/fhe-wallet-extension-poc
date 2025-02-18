07590c13a7f21110aebd941252ec0f787f82dce3
gaspard <gaspard@gmail.com>
2025-02-18 15:42:37 +0100

    feat: remove unused stores

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand'
-import { ethers } from 'ethers'
 import { Transaction } from './types'
 
 type PopupStore = {
@@ -19,28 +18,6 @@ type PopupStore = {
   setGasPrice: (gasPrice: string) => void
 }
 
-type AppStore = {
-  address: string
-  setAddress: (address: string) => void
-  balance: string
-  setBalance: (balance: string) => void
-  encryptedBalance: string
-  setEncryptedBalance: (encryptedBalance: string) => void
-  signer: ethers.Wallet | null
-  setSigner: (signer: ethers.Wallet) => void
-  transactions: Transaction[]
-  setTransactions: (transactions: Transaction[]) => void
-  gasPrice: bigint
-  setGasPrice: (gasPrice: bigint) => void
-}
-
-type AuthStore = {
-  hasWallet: boolean
-  setHasWallet: (hasWallet: boolean) => void
-  walletPrivateKey: string
-  setWalletPrivateKey: (walletPrivateKey: string) => void
-}
-
 export const usePopupStore = create<PopupStore>((set) => ({
   showSettings: false,
   setShowSettings: (showSettings: boolean) => set({ showSettings }),
@@ -56,26 +33,4 @@ export const usePopupStore = create<PopupStore>((set) => ({
   setTransactions: (transactions: Transaction[]) => set({ transactions }),
   gasPrice: '0',
   setGasPrice: (gasPrice: string) => set({ gasPrice }),
-}))
-
-export const useAppStore = create<AppStore>((set) => ({
-  balance: '',
-  setBalance: (balance: string) => set({ balance }),
-  encryptedBalance: '',
-  setEncryptedBalance: (encryptedBalance: string) => set({ encryptedBalance }),
-  address: '',
-  setAddress: (address: string) => set({ address }),
-  signer: null,
-  setSigner: (signer: ethers.Wallet) => set({ signer }),
-  transactions: [],
-  setTransactions: (transactions: Transaction[]) => set({ transactions }),
-  gasPrice: BigInt(0),
-  setGasPrice: (gasPrice: bigint) => set({ gasPrice }),
-}))
-
-export const useAuthStore = create<AuthStore>((set) => ({
-  hasWallet: false,
-  setHasWallet: (hasWallet: boolean) => set({ hasWallet }),
-  walletPrivateKey: '',
-  setWalletPrivateKey: (walletPrivateKey: string) => set({ walletPrivateKey }),
 }))
